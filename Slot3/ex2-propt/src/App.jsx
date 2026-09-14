310c171fbc6b45d5596e71a29b05e778d41e5a1e
phulongtran <phulong280604.com>
2026-09-14 09:09:19 +0700

    feat: add product image with props

--- Slot3/ex2-propt/src/App.jsx
+++ Slot3/ex2-propt/src/App.jsx
@@ -8,12 +8,14 @@ function App() {
         name="iPhone 15"
         price="20.000.000 VNĐ"
         tag="Điện thoại"
+        image="/iphone15.jpg"
       />
 
       <ProductInfo 
         name="Laptop Dell"
         price="25.000.000 VNĐ"
         tag="Laptop"
+        image="/laptop.jpg"
       />
     </>
   )
--- Slot3/ex2-propt/src/ProductInfo.jsx
+++ Slot3/ex2-propt/src/ProductInfo.jsx
@@ -1,6 +1,12 @@
 function ProductInfo(props) {
   return (
     <div>
+      <img 
+        src={props.image} 
+        alt={props.name}
+        width="200"
+      />
+
       <h2>{props.name}</h2>
       <p>Giá: {props.price}</p>
       <p>Thẻ tag: {props.tag}</p>
