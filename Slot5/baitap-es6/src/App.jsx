8dd59917d2986dec1f8c093ff6999b5f693c801e
phulongtran <phulong280604.com>
2026-09-21 08:27:56 +0700

    feat(welcome-card): add student welcome card

---
+++ Slot5/baitap-es6/src/App.jsx
@@ -0,0 +1,12 @@
+import WelcomeCard from './components/WelcomeCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container my-4">
+      <WelcomeCard />
+    </div>
+  );
+}
+
+export default App;
